cfa043354aaf38d3851847306a03390dd73fc226
Eric W. Greene <eric@ericwgreene.com>
2016-03-16 22:11:21 -0500

    updated startup file

--- assets/startup.sh
+++ assets/startup.sh
@@ -10,3 +10,7 @@ sed -i "s/%hostname%/$HOSTNAME/g" "${LISTENERS_ORA}" &&
 sed -i "s/%port%/1521/g" "${LISTENERS_ORA}" &&
 
 service oracle-xe start
+
+export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+
+$ORACLE_HOME/bin/sqlplus system/oracle@localhost < /opt/app/init.sql
